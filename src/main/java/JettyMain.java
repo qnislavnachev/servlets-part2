0f678ac985aa8d8e807945f8216a632b1155bff4
Vasil <v.mitov.clouway@gmail.com>
2016-11-28 09:51:35 +0200

    Initial commit for counter realized with servlets.

---
+++ src/main/java/JettyMain.java
@@ -0,0 +1,35 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * @author Vasil Mitov <v.mitov.clouway@gmail.com>
+ */
+public class JettyMain {
+  public static void main(String[] args) {
+
+    Server server = new Server(8080);
+    ServletContextHandler servletContext = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    servletContext.setContextPath("/");
+    servletContext.addEventListener(new ServletContextListener() {
+      @Override
+      public void contextInitialized(ServletContextEvent servletContextEvent) {
+        ServletContext servletContext = servletContextEvent.getServletContext();
+        }
+
+      @Override
+      public void contextDestroyed(ServletContextEvent servletContextEvent) {
+      }
+    });
+    server.setHandler(servletContext);
+    try {
+      server.start();
+      server.join();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
