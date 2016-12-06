7061409193b67108e4eaf2364a0d00da1784a50e
Petar Nedelchev <peter.krasimirov@gmail.com>
2016-12-06 18:41:45 +0200

    Counter Servlet with tests

---
+++ src/main/java/Demo.java
@@ -0,0 +1,9 @@
+/**
+ * @author Petar Nedelchev (peter.krasimirov@gmail.com)
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Jetty jetty = new Jetty(8080);
+        jetty.start();
+    }
+}
---
+++ src/main/java/Jetty.java
@@ -0,0 +1,43 @@
+import counter.Counter;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * @author Petar Nedelchev (peter.krasimirov@gmail.com)
+ */
+public final class Jetty {
+    private final Server server;
+
+    public Jetty(int port) {
+        this.server = new Server(port);
+    }
+
+    public void start() {
+        ServletContextHandler servletContext = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        servletContext.setContextPath("/");
+
+        servletContext.addEventListener(new ServletContextListener() {
+
+            public void contextInitialized(ServletContextEvent servletContextEvent) {
+                ServletContext servletContext = servletContextEvent.getServletContext();
+
+                servletContext.addServlet("counterServlet", new Counter()).addMapping("/counter");
+            }
+
+            public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+            }
+        });
+
+        server.setHandler(servletContext);
+        try {
+            server.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
